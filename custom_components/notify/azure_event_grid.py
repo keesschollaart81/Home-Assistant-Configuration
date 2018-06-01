33405f92742a16fe587fdbbc4385a5f2a46597ed
Kees Schollaart <keesschollaart81>
2018-06-01 16:40:31 +0200

    Code Review EventGrid notfier

--- custom_components/notify/azure_event_grid.py
+++ custom_components/notify/azure_event_grid.py
@@ -16,7 +16,7 @@ import pytz
 import uuid
 
 from homeassistant.const import (CONF_HOST, CONF_PLATFORM, CONF_NAME)
-from homeassistant.components.notify import (ATTR_DATA, ATTR_TITLE, ATTR_TITLE_DEFAULT, PLATFORM_SCHEMA, BaseNotificationService)
+from homeassistant.components.notify import (ATTR_DATA, PLATFORM_SCHEMA, BaseNotificationService)
 import homeassistant.helpers.config_validation as cv
 from homeassistant.remote import JSONEncoder
 
@@ -25,24 +25,25 @@ REQUIREMENTS = ['azure.eventgrid==0.1.0', 'msrest==0.4.29']
 _LOGGER = logging.getLogger(__name__)
 
 CONF_TOPIC_KEY = 'topic key'
-CONF_CONTEXT = 'context'
+CONF_SUBJECT = 'subject'
+CONF_EVENT_TYPE = 'eventtype'
+CONF_EVENT_TYPE_DEFAULT = 'HomeAssistant'
+CONF_DATA_VERSION = 'dataversion'
+CONF_DATA_VERSION_DEFAULT = 1
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST): cv.string,
-    vol.Required(CONF_TOPIC_KEY): cv.string,
-    vol.Optional(CONF_CONTEXT, default=dict()): vol.Coerce(dict)
+    vol.Inclusive(CONF_TOPIC_KEY,'authentication'): cv.string,
+    vol.Required(CONF_SUBJECT): cv.string,
+    vol.Required(CONF_EVENT_TYPE, default=CONF_EVENT_TYPE_DEFAULT): cv.string,
+    vol.Required(CONF_DATA_VERSION, default=CONF_DATA_VERSION_DEFAULT): vol.Coerce(int),
+    vol.Required(ATTR_DATA): cv.string
 })
 
 def get_service(hass, config, discovery_info=None):
     from azure.eventgrid import EventGridClient
     from msrest.authentication import TopicCredentials
 
-    #create the context dict, puts the HASS config in there as well as a custom piece if configured.
-    context = {
-        'hass': json.loads(json.dumps(hass.config.as_dict(), cls=JSONEncoder)),
-        'custom': config[CONF_CONTEXT]
-        }
-    #create credentials from the key, and create the event_grid_client with the creds.
     credentials = TopicCredentials(config[CONF_TOPIC_KEY])
     event_grid_client = EventGridClient(credentials)
 
@@ -55,24 +56,21 @@ class AzureEventGrid(BaseNotificationService):
         """Initialize the service."""
         self.endpoint = endpoint
         self.client = event_grid_client
-        self.context = context
 
     def send_message(self, message="", **kwargs):
-        title = kwargs.get(ATTR_TITLE, ATTR_TITLE_DEFAULT)
+        subject = kwargs.get(CONF_SUBJECT)
         data = kwargs.get(ATTR_DATA)
+        eventType = kwargs.get(CONF_EVENT_TYPE)
+        dataVersion = kwargs.get(CONF_DATA_VERSION)
 
-        #create the payload, with Title, Message and Data coming in from the notify platform
+        #create the payload, with subject, data and type coming in from the notify platform
         payload = {
             'id' : str(uuid.uuid4()),
-            'subject': title,
-            'data': {
-                'message': message,
-                'data': data,
-                'context': self.context
-            },
-            'event_type': 'HassEventType',
+            'subject': subject,
+            'data': data,
+            'event_type': eventType,
             'event_time': datetime.utcnow().replace(tzinfo=pytz.UTC),
-            'data_version': 1
+            'data_version': dataVersion
         }
 
         #Send the event to event grid
@@ -81,5 +79,5 @@ class AzureEventGrid(BaseNotificationService):
                 self.endpoint,
                 events=[payload]
             )
-        except:
-            _LOGGER.error("Unable to send event to Event Grid")
+        except HomeAssistantError as err:
+            _LOGGER.error("Unable to send event to Event Grid: %s", err)
