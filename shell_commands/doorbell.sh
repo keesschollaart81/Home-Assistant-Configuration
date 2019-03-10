8d8dd75d6e99bd8f810a24d2d529fe2ef5c4dac8
Kees Schollaart <keesschollaart81@hotmail.com>
2019-03-10 14:59:10 +0100

    front-door-gif

--- shell_commands/doorbell.sh
+++ shell_commands/doorbell.sh
@@ -6,6 +6,6 @@ wget -q -O /share/motion/front-door/front2.jpg http://192.168.2.21/jpg
 sleep 3
 wget -q -O /share/motion/front-door/front3.jpg http://192.168.2.21/jpg
 
-convert -loop 0 -delay 100 /share/motion/front-door/front1.jpg /share/motion/front-door/front2.jpg /share/motion/front-door/front3.jpg /share/motion/front-door/front-door-latest.gif
+convert -loop 0 -delay 100 /share/motion/front-door/front1.jpg /share/motion/front-door/front2.jpg /share/motion/front-door/front3.jpg /config/www/front-door/front-door-latest.gif
 
-cp -a /share/motion/front-door/front-door-latest.gif "/share/motion/front-door/front-door-$(date +"%m-%d-%y-%r").gif"
+cp -a /config/www/front-door/front-door-latest.gif "/share/motion/front-door/front-door-$(date +"%m-%d-%y-%r").gif"
