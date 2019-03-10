f885833f4bbbd79b1d65a71a1181e3ab1d336921
Kees Schollaart <keesschollaart81@hotmail.com>
2019-03-10 13:45:49 +0100

    Testing with scripts

---
+++ shell_commands/doorbell.sh
@@ -0,0 +1,16 @@
+ls
+
+pwd
+
+whoami
+
+echo $PATH 
+
+# rm -f /home/homeassistant/axis/*.jpg
+
+# wget -q -O /home/homeassistant/axis/front1.jpg http://***:***@192.168.1.21/jpg/image.jpg
+# sleep 3
+# wget -q -O /home/homeassistant/axis/front2.jpg http://***:***@192.168.1.21/jpg/image.jpg
+# sleep 3
+# wget -q -O /home/homeassistant/axis/front3.jpg http://***:***@192.168.1.21/jpg/image.jpg
+# convert -loop 0 -delay 100 /home/homeassistant/axis/front1.jpg /home/homeassistant/axis/front2.jpg /home/homeassistant/axis/front3.jpg /home/homeassistant/axis/a.gif
