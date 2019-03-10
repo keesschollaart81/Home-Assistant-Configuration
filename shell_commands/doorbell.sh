abf20cf4c0ef8e67fa4f9ea3ba2e65b48be267ca
Kees Schollaart <keesschollaart81@hotmail.com>
2019-03-10 15:28:15 +0100

    more pics

--- shell_commands/doorbell.sh
+++ shell_commands/doorbell.sh
@@ -1,11 +1,19 @@
 find /share/motion/front-door/*.jpg -mtime +7 -exec rm {} \;
 
 wget -q -O /share/motion/front-door/front1.jpg http://192.168.2.21/jpg
-sleep 3
+sleep 1
 wget -q -O /share/motion/front-door/front2.jpg http://192.168.2.21/jpg
-sleep 3
+sleep 1
 wget -q -O /share/motion/front-door/front3.jpg http://192.168.2.21/jpg
+sleep 1
+wget -q -O /share/motion/front-door/front4.jpg http://192.168.2.21/jpg
+sleep 1
+wget -q -O /share/motion/front-door/front5.jpg http://192.168.2.21/jpg
+sleep 1
+wget -q -O /share/motion/front-door/front6.jpg http://192.168.2.21/jpg
+sleep 1
+wget -q -O /share/motion/front-door/front7.jpg http://192.168.2.21/jpg
 
-convert -loop 0 -delay 100 /share/motion/front-door/front1.jpg /share/motion/front-door/front2.jpg /share/motion/front-door/front3.jpg /config/www/front-door/front-door-latest.gif
+convert -loop 0 -delay 100 /share/motion/front-door/front1.jpg /share/motion/front-door/front2.jpg /share/motion/front-door/front3.jpg /share/motion/front-door/front4.jpg /share/motion/front-door/front5.jpg /share/motion/front-door/front6.jpg /share/motion/front-door/front7.jpg /config/www/front-door/front-door-latest.gif
 
 cp -a /config/www/front-door/front-door-latest.gif "/share/motion/front-door/front-door-$(date +"%m-%d-%y-%r").gif"
