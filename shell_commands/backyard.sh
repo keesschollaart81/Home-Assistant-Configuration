784c00cee06917b209fb726f099198b45d9b13be
Kees Schollaart <keesschollaart81@hotmail.com>
2019-03-24 19:43:33 +0100

    remove at the end

--- shell_commands/backyard.sh
+++ shell_commands/backyard.sh
@@ -1,5 +1,3 @@
-find /share/motion/backyard/*.jpg -mtime +2 -exec rm {} \;
-
 wget -q -O /share/motion/backyard/backyard1.jpg http://root:$1@192.168.2.11/cgi-bin/currentpic.cgi
 sleep 1
 wget -q -O /share/motion/backyard/backyard2.jpg http://root:$1@192.168.2.11/cgi-bin/currentpic.cgi
@@ -17,3 +15,5 @@ wget -q -O /share/motion/backyard/backyard7.jpg http://root:$1@192.168.2.11/cgi-
 convert -loop 0 -delay 100 /share/motion/backyard/backyard1.jpg /share/motion/backyard/backyard2.jpg /share/motion/backyard/backyard3.jpg /share/motion/backyard/backyard4.jpg /share/motion/backyard/backyard5.jpg /share/motion/backyard/backyard6.jpg /share/motion/backyard/backyard7.jpg /config/www/backyard/backyard-latest.gif
 
 cp -a /config/www/backyard/backyard-latest.gif "/share/motion/backyard/backyard-$(date +"%m-%d-%y-%r").gif"
+
+find /share/motion/backyard/*.jpg -mtime +2 -exec rm {} \;
