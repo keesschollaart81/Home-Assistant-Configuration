5d92771e7d73a7ff004c20bb21207ab3826ca8c8
Kees Schollaart <keesschollaart81@hotmail.com>
2019-05-18 12:34:04 +0200

    reduce the file size

--- shell_commands/backyard.sh
+++ shell_commands/backyard.sh
@@ -12,7 +12,7 @@ wget -q -O /share/motion/backyard/backyard6.jpg http://root:$1@192.168.2.11/cgi-
 sleep 1
 wget -q -O /share/motion/backyard/backyard7.jpg http://root:$1@192.168.2.11/cgi-bin/currentpic.cgi
 
-convert -loop 0 -delay 100 /share/motion/backyard/backyard1.jpg /share/motion/backyard/backyard2.jpg /share/motion/backyard/backyard3.jpg /share/motion/backyard/backyard4.jpg /share/motion/backyard/backyard5.jpg /share/motion/backyard/backyard6.jpg /share/motion/backyard/backyard7.jpg /config/www/backyard/backyard-latest.gif
+convert -loop 0 -delay 100 /share/motion/backyard/backyard1.jpg /share/motion/backyard/backyard2.jpg /share/motion/backyard/backyard3.jpg /share/motion/backyard/backyard4.jpg /share/motion/backyard/backyard5.jpg /share/motion/backyard/backyard6.jpg /share/motion/backyard/backyard7.jpg -fuzz 80% -layers Optimize /config/www/backyard/backyard-latest.gif
 
 cp -a /config/www/backyard/backyard-latest.gif "/share/motion/backyard/backyard-$(date +"%m-%d-%y-%r").gif"
 
