ea7b17f9da4655e6eefbab831d1bbb0ac7159081
Kees Schollaart <keesschollaart81@hotmail.com>
2019-05-18 14:31:45 +0200

    80 > 15

--- shell_commands/backyard.sh
+++ shell_commands/backyard.sh
@@ -14,7 +14,7 @@ wget -q -O /share/motion/backyard/backyard7.jpg http://root:$1@192.168.2.11/cgi-
 
 convert -loop 0 -delay 100 /share/motion/backyard/backyard1.jpg /share/motion/backyard/backyard2.jpg /share/motion/backyard/backyard3.jpg /share/motion/backyard/backyard4.jpg /share/motion/backyard/backyard5.jpg /share/motion/backyard/backyard6.jpg /share/motion/backyard/backyard7.jpg -fuzz 80% -layers Optimize /config/www/backyard/backyard-latest.gif
 
-convert /config/www/backyard/backyard-latest.gif -fuzz 80% -layers Optimize /config/www/backyard/backyard-latest-small.gif
+convert /config/www/backyard/backyard-latest.gif -fuzz 15% -layers Optimize /config/www/backyard/backyard-latest-small.gif
 
 cp -a /config/www/backyard/backyard-latest-small.gif "/share/motion/backyard/backyard-$(date +"%m-%d-%y-%r").gif"
 
--- shell_commands/front-door.sh
+++ shell_commands/front-door.sh
@@ -14,7 +14,7 @@ wget -q -O /share/motion/front-door/front7.jpg https://schollaartthuis.duckdns.o
 
 convert -loop 0 -delay 100 /share/motion/front-door/front1.jpg /share/motion/front-door/front2.jpg /share/motion/front-door/front3.jpg /share/motion/front-door/front4.jpg /share/motion/front-door/front5.jpg /share/motion/front-door/front6.jpg /share/motion/front-door/front7.jpg /config/www/front-door/front-door-latest.gif 
 
-convert /config/www/front-door/front-door-latest.gif -fuzz 80% -layers Optimize  /config/www/front-door/front-door-latest-small.gif
+convert /config/www/front-door/front-door-latest.gif -fuzz 15% -layers Optimize  /config/www/front-door/front-door-latest-small.gif
 
 cp -a /config/www/front-door/front-door-latest-small.gif "/share/motion/front-door/front-door-$(date +"%m-%d-%y-%r").gif"
 
