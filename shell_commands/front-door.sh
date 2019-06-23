8aeced57076476e27e8dc60c94fb2434dde269f7
Kees Schollaart <keesschollaart81@hotmail.com>
2019-06-23 20:47:24 +0200

    front door gif renewed

--- shell_commands/front-door.sh
+++ shell_commands/front-door.sh
@@ -1,17 +1,3 @@
-wget -q -O /share/motion/front-door/front1.jpg https://schollaartthuis.duckdns.org:8123$1
-sleep 1
-wget -q -O /share/motion/front-door/front2.jpg https://schollaartthuis.duckdns.org:8123$1
-sleep 1
-wget -q -O /share/motion/front-door/front3.jpg https://schollaartthuis.duckdns.org:8123$1
-sleep 1
-wget -q -O /share/motion/front-door/front4.jpg https://schollaartthuis.duckdns.org:8123$1
-sleep 1
-wget -q -O /share/motion/front-door/front5.jpg https://schollaartthuis.duckdns.org:8123$1
-sleep 1
-wget -q -O /share/motion/front-door/front6.jpg https://schollaartthuis.duckdns.org:8123$1
-sleep 1
-wget -q -O /share/motion/front-door/front7.jpg https://schollaartthuis.duckdns.org:8123$1
-
 convert -loop 0 -delay 100 /share/motion/front-door/front1.jpg /share/motion/front-door/front2.jpg /share/motion/front-door/front3.jpg /share/motion/front-door/front4.jpg /share/motion/front-door/front5.jpg /share/motion/front-door/front6.jpg /share/motion/front-door/front7.jpg /config/www/front-door/front-door-latest.gif 
 
 convert /config/www/front-door/front-door-latest.gif -fuzz 15% -layers Optimize  /config/www/front-door/front-door-latest-small.gif
