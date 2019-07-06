a902d6772d2bf334ac15c6864081cff999c47545
Kees Schollaart <keesschollaart81@hotmail.com>
2019-07-06 13:12:58 +0200

    updated take_front_door_snaps

--- shell_commands/front-door.sh
+++ shell_commands/front-door.sh
@@ -1,4 +1,4 @@
-convert -loop 0 -delay 100 /share/motion/front-door/front1.jpg /share/motion/front-door/front2.jpg /share/motion/front-door/front3.jpg /share/motion/front-door/front4.jpg /share/motion/front-door/front5.jpg /share/motion/front-door/front6.jpg /share/motion/front-door/front7.jpg /config/www/front-door/front-door-latest.gif 
+convert -loop 0 -delay 100 /share/motion/front-door/front1.jpg /share/motion/front-door/front2.jpg /share/motion/front-door/front3.jpg /share/motion/front-door/front4.jpg /share/motion/front-door/front5.jpg /share/motion/front-door/front6.jpg /share/motion/front-door/front7.jpg /share/motion/front-door/front8.jpg /share/motion/front-door/front9.jpg /config/www/front-door/front-door-latest.gif 
 
 convert /config/www/front-door/front-door-latest.gif -fuzz 15% -layers Optimize  /config/www/front-door/front-door-latest-small.gif
 
